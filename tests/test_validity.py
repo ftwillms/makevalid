d17b9da508a5d6ded9aa167ccf4edd13ba980124
Forrest Williams <forrest.williams@gmail.com>
2015-04-13 09:35:58 -0500

    Refer to relative package when testing

--- tests/test_validity.py
+++ tests/test_validity.py
@@ -4,6 +4,8 @@ import logging
 import sys
 logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
 
+from .. import makevalid
+
 
 class ValidityTestCases(unittest.TestCase):
 
@@ -13,12 +15,10 @@ class ValidityTestCases(unittest.TestCase):
         from shapely.geometry import Polygon
         from shapely.geometry import MultiPolygon
 
-        from makevalid import make_geom_valid
-
         output_results = []
         print("Testing valid, plain polygon...")
         test_poly = shapely.wkt.loads('POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))')
-        validated_poly = make_geom_valid(test_poly)
+        validated_poly = makevalid.make_geom_valid(test_poly)
         self.assertIsInstance(validated_poly, Polygon)
         self.assertTrue(test_poly.length == validated_poly.length)
         self.assertTrue(test_poly.area == validated_poly.area)
@@ -26,7 +26,7 @@ class ValidityTestCases(unittest.TestCase):
 
         print("Testing valid polygon with interior hole...")
         test_poly = shapely.wkt.loads('POLYGON ((35 10, 45 45, 15 40, 10 20, 35 10),(20 30, 35 35, 30 20, 20 30))')
-        validated_poly = make_geom_valid(test_poly)
+        validated_poly = makevalid.make_geom_valid(test_poly)
         self.assertIsInstance(validated_poly, Polygon)
         self.assertTrue(test_poly.length == validated_poly.length)
         self.assertTrue(test_poly.area == validated_poly.area)
@@ -34,14 +34,14 @@ class ValidityTestCases(unittest.TestCase):
 
         print("Testing invalid missing quadrant polygon...")
         test_poly = shapely.wkt.loads('POLYGON ((0 0, 0 2, 2 2, 2 0, 0 0), (0 0, 0 1, 1 1, 1 0, 0 0))')
-        validated_poly = make_geom_valid(test_poly)
+        validated_poly = makevalid.make_geom_valid(test_poly)
         self.assertTrue(validated_poly.is_valid)
         self.assertTrue(validated_poly.area == 4.0)
         output_results.append({'properties': {'name': 'missing_quadrant'}, 'geometry': mapping(validated_poly)})
 
         print("Testing classic, invalid bowtie polygon...")
         test_poly = shapely.wkt.loads('POLYGON((0 0, 0 10, 10 0, 10 10, 0 0))')
-        validated_poly = make_geom_valid(test_poly)
+        validated_poly = makevalid.make_geom_valid(test_poly)
         self.assertTrue(validated_poly.is_valid)
         self.assertIsInstance(validated_poly, MultiPolygon)
         self.assertTrue(len(validated_poly) == 2)
@@ -50,7 +50,7 @@ class ValidityTestCases(unittest.TestCase):
 
         print("Testing bowtie with a hole in it!")
         test_poly = shapely.wkt.loads('POLYGON((0 0, 0 10, 10 0, 10 10, 0 0), (9 8, 6 5, 9 2, 9 8))')
-        validated_poly = make_geom_valid(test_poly)
+        validated_poly = makevalid.make_geom_valid(test_poly)
         self.assertTrue(validated_poly.is_valid)
         self.assertTrue(len(validated_poly) == 2)
         self.assertTrue(len(validated_poly[0].interiors) == 0)
@@ -59,7 +59,7 @@ class ValidityTestCases(unittest.TestCase):
 
         print("Testing hole outside shell...")
         test_poly = shapely.wkt.loads('POLYGON((0 0, 10 0, 10 10, 0 10, 0 0), (15 15, 15 20, 20 20, 20 15, 15 15))')
-        validated_poly = make_geom_valid(test_poly)
+        validated_poly = makevalid.make_geom_valid(test_poly)
         self.assertTrue(validated_poly.is_valid)
         self.assertTrue(len(validated_poly) == 2)
         output_results.append({'properties': {'name': 'hole_outside_shell'}, 'geometry': mapping(validated_poly)})
@@ -68,21 +68,21 @@ class ValidityTestCases(unittest.TestCase):
         print("Testing nested holes...")
         test_poly = shapely.wkt.loads('POLYGON((0 0, 10 0, 10 10, 0 10, 0 0), (2 2, 2 8, 8 8, 8 2, 2 2), '
                                       '(3 3, 3 7, 7 7, 7 3, 3 3))')
-        validated_poly = make_geom_valid(test_poly)
+        validated_poly = makevalid.make_geom_valid(test_poly)
         self.assertTrue(validated_poly.is_valid)
         self.assertTrue(len(validated_poly) == 2)
         output_results.append({'properties': {'name': 'nested_holes'}, 'geometry': mapping(validated_poly)})
 
         print("Testing disconnected interior...")
         test_poly = shapely.wkt.loads('POLYGON((0 0, 10 0, 10 10, 0 10, 0 0), (5 0, 10 5, 5 10, 0 5, 5 0))')
-        validated_poly = make_geom_valid(test_poly)
+        validated_poly = makevalid.make_geom_valid(test_poly)
         self.assertTrue(validated_poly.is_valid)
         self.assertTrue(len(validated_poly) == 4)
         output_results.append({'properties': {'name': 'disconnected_interior'}, 'geometry': mapping(validated_poly)})
 
         print("Testing ring self-intersection...")
         test_poly = shapely.wkt.loads('POLYGON((5 0, 10 0, 10 10, 0 10, 0 0, 5 0, 3 3, 5 6, 7 3, 5 0))')
-        validated_poly = make_geom_valid(test_poly)
+        validated_poly = makevalid.make_geom_valid(test_poly)
         self.assertIsInstance(validated_poly, Polygon)
         self.assertTrue(validated_poly.is_valid)
         self.assertTrue(len(validated_poly.interiors) == 1)
@@ -90,7 +90,7 @@ class ValidityTestCases(unittest.TestCase):
 
         print("Testing nested shells...")
         test_poly = shapely.wkt.loads('MULTIPOLYGON(((0 0, 10 0, 10 10, 0 10, 0 0)),(( 2 2, 8 2, 8 8, 2 8, 2 2)))')
-        validated_poly = make_geom_valid(test_poly)
+        validated_poly = makevalid.make_geom_valid(test_poly)
         self.assertIsInstance(validated_poly, Polygon)
         self.assertTrue(validated_poly.is_valid)
         self.assertTrue(validated_poly.area == 64.0)
