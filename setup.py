ff134faa97163f92c6fd60dd454dd03ff784001e
Forrest Williams <forrest.williams@gmail.com>
2018-03-01 10:30:43 -0600

    Update the appropriate license information

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     author='Forrest Williams',
     author_email='forrest.williams@gmail.com',
     url='https://github.com/ftwillms/makevalid',
-    description='Using the Shapely module, function to make a polygon valid.',
-    license='BSD',
+    description='Using the Shapely module, function to make a polygon valid based on PostGIS ST_MakeValid.',
+    license='GPLv2+',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -20,7 +20,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
         'Topic :: Scientific/Engineering :: GIS',
     ],
 )
